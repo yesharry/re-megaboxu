3667f515d609508820ceb97594d329f9b760a853
yesharry <reerohh@gmail.com>
2022-11-13 15:27:02 +0900

    footer 리팩토링

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+import FooterTop from './components/FooterTop/FooterTop';
+import FooterBottom from './components/FooterBottom/FooterBottom';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <FooterWrapper>
+      <FooterTop />
+      <FooterBottom />
+    </FooterWrapper>
+  );
 };
+
+const FooterWrapper = styled.div`
+  width: 100%;
+  height: 12.5rem;
+  background-color: ${props => props.theme.colors.lightGray};
+`;
 
 export default Footer;
---
+++ src/components/Footer/components/FooterTop/FooterTop.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FindTheater from './components/FindTheater';
+import MegaboxuInfo from './components/MegaboxuInfo';
+
+const FooterTop = () => {
+  return (
+    <>
+      <MegaboxuInfo />
+      <FindTheater />
+    </>
+  );
+};
+
+export default FooterTop;
